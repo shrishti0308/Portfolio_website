ae99a9c44c5d6fd26542bd559ef6f2b8c85a5011
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2024-12-17 17:33:28 +0530

    add Navbar component with logo and social media icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/shrishtiLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaTwitterSquare />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
